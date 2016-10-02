50eff060d23eb8a044c582bce291de906cc33e0c
maryna <kushneruk_1995@mail.ru>
2016-10-02 22:24:49 +0300

    version two

--- script/task.js
+++ script/task.js
@@ -11,48 +11,49 @@ clear = document.querySelector('.clear');
 display = document.querySelector('.timer');
 msDisplay = document.querySelector('.miliseconds');
 
-function doTimer() {
-  if (runTimer == 0) {
-    runTimer = 1;
-    timedCount();
-    startPauseContinue.style.background = "rgb(66, 184, 221)";
-  }
-  else {
-    clearTimeout(t); 
-    runTimer = 0; 
-    startPauseContinue.innerHTML = "Continue";
-    startPauseContinue.style.background = "rgb(28, 184, 65)";
-  }
-}
 
-function timedCount() {
 
-  miliSeconds++;
-  if(miliSeconds >= 99) {
-    miliSeconds = 0;
-    seconds++;
-    if (seconds >= 60) {
-      seconds = 0;
-      minutes++;
-      if(minutes >= 60) {
-        minutes = 0;
-        hours++;
-        if(hours >= 24) {
-          hours = 0;
+var timer = {
+  doTimer: function doTimer() {
+    if (runTimer == 0) {
+      runTimer = 1;
+      t = setInterval("timer.timedCount()", 10);
+      startPauseContinue.style.background = "rgb(66, 184, 221)";
+    }
+    else {
+      clearInterval(t); 
+      runTimer = 0; 
+      startPauseContinue.innerHTML = "Continue";
+      startPauseContinue.style.background = "rgb(28, 184, 65)";
+    }
+  },
+
+  timedCount : function timedCount() {
+
+    miliSeconds++;
+    if(miliSeconds > 99) {
+      miliSeconds = 0;
+      seconds++;
+      if (seconds > 60) {
+        seconds = 0;
+        minutes++;
+        if(minutes > 60) {
+          minutes = 0;
+          hours++;
+          if(hours > 24) {
+            hours = 0;
+          }
         }
       }
     }
-  }
 
-  t = setTimeout("timedCount()", 10);
+    startPauseContinue.innerHTML = "Pause";
+    display.innerHTML = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+    msDisplay.innerHTML = (miliSeconds ? (miliSeconds > 9 ? miliSeconds : "0" + hours) : "00") ;
+  },
 
-  startPauseContinue.innerHTML = "Pause";
-  display.innerHTML = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-  msDisplay.innerHTML = (miliSeconds ? (miliSeconds > 9 ? miliSeconds : "0" + hours) : "00") ;
-}
-
-function clearCount() { 
-  clearTimeout(t); 
+clearCount: function clearCount() { 
+  clearInterval(t); 
   display.textContent = "00:00:00";
   msDisplay.textContent = "0";
   seconds = 0;
@@ -63,6 +64,11 @@ function clearCount() {
   startPauseContinue.innerHTML = "Start";
   startPauseContinue.style.background = "rgb(28, 184, 65)";
 }
+}
+
+
+
+
 
   
 
