4e82acfe8af7b03cc7123a579869461cd08e75e0
maryna <kushneruk_1995@mail.ru>
2016-10-03 22:35:22 +0300

    version three

--- script/task.js
+++ script/task.js
@@ -3,7 +3,8 @@ var seconds = 0;
 var minutes = 0;
 var hours = 0;
 var t;
-var runTimer=0;
+var runTimer = 0;
+var time = 0;
 
 
 startPauseContinue = document.querySelector('.start');
@@ -17,8 +18,8 @@ var timer = {
   doTimer: function doTimer() {
     if (runTimer == 0) {
       runTimer = 1;
-      t = setInterval("timer.timedCount()", 10);
       startPauseContinue.style.background = "rgb(66, 184, 221)";
+      t = setInterval("timer.timedCount()", 10);
     }
     else {
       clearInterval(t); 
@@ -28,42 +29,27 @@ var timer = {
     }
   },
 
-  timedCount : function timedCount() {
-
-    miliSeconds++;
-    if(miliSeconds > 99) {
-      miliSeconds = 0;
-      seconds++;
-      if (seconds > 60) {
-        seconds = 0;
-        minutes++;
-        if(minutes > 60) {
-          minutes = 0;
-          hours++;
-          if(hours > 24) {
-            hours = 0;
-          }
-        }
-      }
-    }
+  timedCount: function timedCount() {
+    time++;
+    miliSeconds = time%100;
+    seconds = Math.floor((time/100)%60);
+    minutes = Math.floor((time/6000)%60);
+    hours = Math.floor((time/360000)%24);
 
     startPauseContinue.innerHTML = "Pause";
     display.innerHTML = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
-    msDisplay.innerHTML = (miliSeconds ? (miliSeconds > 9 ? miliSeconds : "0" + hours) : "00") ;
+    msDisplay.innerHTML = (miliSeconds ? (miliSeconds > 9 ? miliSeconds : "0" + miliSeconds) : "00") ;
   },
 
-clearCount: function clearCount() { 
-  clearInterval(t); 
-  display.textContent = "00:00:00";
-  msDisplay.textContent = "0";
-  seconds = 0;
-  minutes = 0;
-  hours = 0;
-  miliSeconds = 0;
-  runTimer = 0;
-  startPauseContinue.innerHTML = "Start";
-  startPauseContinue.style.background = "rgb(28, 184, 65)";
-}
+  clearCount: function clearCount() { 
+    clearInterval(t); 
+    display.textContent = "00:00:00";
+    msDisplay.textContent = "0";
+    runTimer = 0;
+    time = 0;
+    startPauseContinue.innerHTML = "Start";
+    startPauseContinue.style.background = "rgb(28, 184, 65)";
+  }
 }
 
 
